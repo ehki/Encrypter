e482e3dfe480f002a34084d6c91f169d59d69180
ehki <0y35.ejiri.vmqewyhw@gmail.com>
2021-10-19 18:32:06 +0900

    Add suggestion to password box

--- crypttools/tkmgr.py
+++ crypttools/tkmgr.py
@@ -38,6 +38,39 @@ def decrypt(key, source, decode=True):
     return data[:-padding]
 
 
+class EntryWithPlaceholder(Entry):
+    def __init__(
+            self,
+            master=None,
+            placeholder="PLACEHOLDER",
+            color='grey',
+            **options):
+        super().__init__(master, **options)
+
+        self.placeholder = placeholder
+        self.placeholder_color = color
+        self.default_fg_color = self['fg']
+
+        self.bind("<FocusIn>", self.foc_in)
+        self.bind("<FocusOut>", self.foc_out)
+
+        self.put_placeholder()
+
+    def put_placeholder(self):
+        self.insert(0, self.placeholder)
+        self['fg'] = self.placeholder_color
+
+    def foc_in(self, *args):
+        if self['fg'] == self.placeholder_color:
+            self.delete('0', 'end')
+            self['fg'] = self.default_fg_color
+            self.config(show='*')
+
+    def foc_out(self, *args):
+        if not self.get():
+            self.put_placeholder()
+
+
 class Window():
     def __init__(self) -> None:
 
@@ -74,7 +107,7 @@ class Window():
         self.encrypt_button.pack()
         self.decrypt_button.pack()
         self.exit_button.pack()
-        self.pswdbox = Entry(self.passframe, show='*')
+        self.pswdbox = EntryWithPlaceholder(self.passframe, 'password')
         self.pswdbox.pack()
   
     def encrypt(self, text, pswd) -> None:
